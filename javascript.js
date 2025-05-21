1b9f498d1629778e07a1be96ee63121c93693282
Rizky2011 <144513571+Rizky2011@users.noreply.github.com>
2025-05-21 10:39:21 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,8 @@
-- 👋 Hi, I’m @Rizky2011
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
-
-<!---
-Rizky2011/Rizky2011 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+function handleLoginTap() {
+  Redirect ke halaman login
+  Membuka modal login
+  Mengirimkan permintaan ke server untuk login
+  window.location.href = "login.html";
+  console.log("Login button tapped!")
+    Menampilkan pesan di console
+}
