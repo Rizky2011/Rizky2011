5435e6e7eff2c1501a61ccdba1036b9f8d1cf33a
Rizky2011 <144513571+Rizky2011@users.noreply.github.com>
2025-06-04 06:39:58 +0700

    Update javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function handleLoginTap() {
-  Redirect ke halaman login
-  Membuka modal login
-  Mengirimkan permintaan ke server untuk login
-  window.location.href = "login.html";
-  console.log("Login button tapped!")
-    Menampilkan pesan di console
-}
