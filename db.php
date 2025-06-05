244296929265d4e3259757812feed34f67a2678b
Rizky2011 <144513571+Rizky2011@users.noreply.github.com>
2025-06-05 11:46:11 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'your_database';
+$user = 'your_username';
+$pass = 'your_password';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die('Koneksi gagal: ' . $conn->connect_error);
+}
+?>
