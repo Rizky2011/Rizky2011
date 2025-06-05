ff8f664e2df124d7df4bd5376e58e5feb86d63fe
Rizky2011 <144513571+Rizky2011@users.noreply.github.com>
2025-06-05 11:50:29 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
 $db = 'your_database';
-$user = 'your_username';rizky123
-$pass = 'your_password';2011
+$user = 'your_username';
+$pass = 'your_password';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
